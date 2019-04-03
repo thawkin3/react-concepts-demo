324434b5210bd5bfd9919abcc02ee0b045671895
Tyler Hawkins <thawkins@instructure.com>
2019-04-02 20:59:14 -0600

    first build: refs and error boundaries

---
+++ build/precache-manifest.e49c1812dfde0207d56302ef3e0bfc2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a6bb1bc55604e34147d",
+    "url": "/react-concepts-demo/build/static/css/main.426568c7.chunk.css"
+  },
+  {
+    "revision": "1a6bb1bc55604e34147d",
+    "url": "/react-concepts-demo/build/static/js/main.16cfece9.chunk.js"
+  },
+  {
+    "revision": "357e46a223892ecafa17",
+    "url": "/react-concepts-demo/build/static/js/runtime~main.8d371768.js"
+  },
+  {
+    "revision": "40f876d9c794d61a0afa",
+    "url": "/react-concepts-demo/build/static/js/2.f50f7a78.chunk.js"
+  },
+  {
+    "revision": "846c7089ae016d67eed05ad3093b9246",
+    "url": "/react-concepts-demo/build/index.html"
+  }
+];
